80789028d68e0143dd6c3c87391eb6475ca11a71
DerEchtePilz <tlfm90@gmail.com>
2022-04-08 21:34:26 +0200

    fixing plugin.yml and additions

--- src/main/java/me/derechtepilz/economy/itemmanager/ItemBuyOffer.java
+++ src/main/java/me/derechtepilz/economy/itemmanager/ItemBuyOffer.java
@@ -1,5 +1,19 @@
 package me.derechtepilz.economy.itemmanager;
 
-public class ItemBuyOffer {
+import dev.jorel.commandapi.CommandAPICommand;
+import me.derechtepilz.economy.Main;
+import org.bukkit.entity.Player;
 
+public class ItemBuyOffer {
+    public ItemBuyOffer() {
+        new CommandAPICommand("buy")
+                .executes((sender, args) -> {
+                    if (!(sender instanceof Player player)) {
+                        sender.sendMessage("§cYou cannot execute this command!");
+                        return;
+                    }
+
+                })
+                .register();
+    }
 }
