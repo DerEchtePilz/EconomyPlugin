2d4c192118d461bce9df39d3e1e0057730838ca5
DerEchtePilz <tlfm90@gmail.com>
2022-11-19 13:28:07 +0100

    remove method body for the trade system

--- economy-plugin/src/main/java/io/github/derechtepilz/economy/tradesystem/Trade.kt
+++ economy-plugin/src/main/java/io/github/derechtepilz/economy/tradesystem/Trade.kt
@@ -165,10 +165,6 @@ class Trade(private val main: Main, private val tradeInitiator: Player, private 
         return -1
     }
 
-    private fun resortInventory(itemStack: ItemStack, inventoryContents: Array<ItemStack?>, tradeSlots: IntArray) {
-
-    }
-
     private fun cancelTrade() {
         tradeInitiator.inventory.addItem(*itemsForTarget.toTypedArray())
         tradeTarget.inventory.addItem(*itemsForPlayer.toTypedArray())
